21b9a086071ab6d1475d3350a1fc0a885b71fb2c
Manos <manzar@image.ece.ntua.gr>
2020-05-14 20:23:37 +0300

    init commit gtp2 collator

--- core/data/collators.py
+++ core/data/collators.py
@@ -38,3 +38,38 @@ class Bert2BertCollator(object):
 
         return padded_inputs, inputs_lengths, inputs_pad_mask, padded_targets, \
                targets_lengths, targets_pad_mask
+
+
+class GPT2Collator(object):
+    def __init__(self, pad_indx=0, device='cpu'):
+        self.pad_indx = pad_indx
+        self.device = device
+
+    def __call__(self, batch):
+        inputs, targets, labels = map(list, zip(*batch))
+        inputs_lengths = torch.tensor(
+            [len(s) for s in inputs], device=self.device)
+
+        targets_lengths = torch.tensor(
+            [len(s) for s in targets], device=self.device)
+
+        # create attention masks
+        max_length = max(inputs_lengths)
+        inputs_pad_mask = pad_mask(inputs_lengths, max_length=max_length,
+                       device=self.device)
+        max_length = max(targets_lengths)
+        targets_pad_mask = pad_mask(targets_lengths, max_length=max_length,
+                       device=self.device)
+        sub_m = subsequent_mask(max_length)
+
+        # Pad inputs and targets
+        padded_inputs = (
+            pad_sequence(inputs, batch_first=True, padding_value=self.pad_indx)
+                .to(self.device))
+        padded_targets = (
+            pad_sequence(targets, batch_first=True,
+                         padding_value=self.pad_indx)
+                .to(self.device))
+
+        return padded_inputs, inputs_lengths, inputs_pad_mask, padded_targets, \
+               targets_lengths, targets_pad_mask
