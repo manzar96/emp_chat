2694c9ea1908dfb3075640bfb3c1e762ccb33f39
manzar96 <manoszar96@gmail.com>
2020-05-27 02:37:31 +0300

    implemented sequence cross entropy loss

---
+++ core/modules/loss.py
@@ -0,0 +1,13 @@
+import torch.nn as nn
+import torch
+
+
+class SequenceCrossEntropyLoss(nn.Module):
+    def __init__(self, pad_idx=0):
+        super(SequenceCrossEntropyLoss, self).__init__()
+        self.criterion = nn.CrossEntropyLoss(ignore_index=pad_idx)
+
+    def forward(self, y_pred, targets):
+        y_pred = y_pred.contiguous().view(-1, y_pred.size(-1))
+        targets = targets.contiguous().view(-1)
+        return self.criterion(y_pred, targets)
