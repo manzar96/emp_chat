b8bb515fb93a57e8b3d63c0c060fbae6c4031d73
manzar96 <manoszar96@gmail.com>
2020-09-30 03:22:01 +0300

    triple loss multitask t5

--- core/models/huggingface/t5_extended.py
+++ core/models/huggingface/t5_extended.py
@@ -31,3 +31,37 @@ class T5ConditionalGenerationDoubleHead(nn.Module):
         enc_last_hidden_last_timestep = enc_last_hidden[:,-1,:]
         clf_logits = self.clf_layer(enc_last_hidden_last_timestep)
         return lm_loss, lm_logits, clf_logits
+
+
+class T5ConditionalGenerationTripleHead(nn.Module):
+    """
+    This is T5 model with 2 heads.
+    An LM head + a classification head
+    """
+    def __init__(self, model_version, num_classes=3, device='cpu'):
+        super(T5ConditionalGenerationTripleHead, self).__init__()
+        self.num_classes = num_classes
+        self.lm_model = T5ForConditionalGeneration.from_pretrained(model_version)
+        self.clf_layer_enc = nn.Linear(in_features=self.lm_model.config.d_model,
+                                   out_features=num_classes)
+        self.clf_layer_dec = nn.Linear(in_features=self.lm_model.config.d_model,
+                                       out_features=num_classes)
+        self.device = device
+
+    def forward(self, *args, **kwargs):
+
+        emo_label = kwargs['emolabel']
+        kwargs.pop('emolabel', None)
+        outputs = self.lm_model(**kwargs, output_hidden_states=True,
+                                return_dict=True)
+        lm_loss = outputs['loss']
+        lm_logits = outputs['logits']
+        dec_hidden_states = outputs['decoder_hidden_states']
+        enc_last_hidden = outputs['encoder_last_hidden_state']
+        enc_hidden_states = outputs['encoder_hidden_states']
+        last_dec_hidden = dec_hidden_states[-1]
+        enc_last_hidden_last_timestep = enc_last_hidden[:,-1,:]
+        last_dec_hidden_last_timestep = last_dec_hidden[:,-1,:]
+        clf_logits_enc = self.clf_layer_enc(enc_last_hidden_last_timestep)
+        clf_logits_dec = self.clf_layer_dec(last_dec_hidden_last_timestep)
+        return lm_loss, lm_logits, clf_logits_enc, clf_logits_dec
