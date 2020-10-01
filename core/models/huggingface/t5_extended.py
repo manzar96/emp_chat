0705d1adf27663fbe8289aaa4e2337348ffc8e9e
manzar96 <manoszar96@gmail.com>
2020-10-01 15:40:47 +0300

    relu on classification

--- core/models/huggingface/t5_extended.py
+++ core/models/huggingface/t5_extended.py
@@ -17,9 +17,12 @@ class T5ConditionalGenerationDoubleHead(nn.Module):
         self.clf_enc = nn.Sequential(
             nn.Linear(in_features=self.lm_model.config.d_model,
                       out_features=300,bias=True),
-            nn.GELU(),
+            # nn.Dropout(0.1),
+            nn.BatchNorm1d(300),  # applying batch norm
+            nn.ReLU(),
             nn.Linear(in_features=300,
                       out_features=self.num_classes)
+
         )
         self.device = device
 
