2491fdc194d6040eec724174586064758cb04156
manzar96 <manoszar96@gmail.com>
2020-10-02 01:21:57 +0300

    t5multi

--- experiments/multitask/test_t5_multitask.py
+++ experiments/multitask/test_t5_multitask.py
@@ -179,7 +179,7 @@ model.lm_model.config.dropout_rate = 0
 
 import ipdb;ipdb.set_trace()
 # generate answers model
-#_generate(options, model, test_loader, tokenizer, DEVICE)
+_generate(options, model, test_loader, tokenizer, DEVICE)
 
 # calc and print metrics
 # calc_test_ppl(model, test_loader, DEVICE)
